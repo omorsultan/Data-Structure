55cd64c1e29dcf96089f3a17e5bec7fbfced0a50
omorsultan <omorsultansust@gmail.com>
2025-05-18 11:08:43 +0600

    for checking diffrence

--- RAW CODE/BellmanFord/implementBellman/singleSource1.cpp
+++ RAW CODE/BellmanFord/implementBellman/singleSource1.cpp
@@ -1,12 +1,14 @@
+// https://open.kattis.com/problems/shortestpath3
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
-vector<vector<ll>>g;
-ll inf = 1e18+10;
 const ll N = 1e6+10;
-ll n,m;
+vector<vector<ll >>g;
+ll inf = 1e18+10;
 vector<ll>dist(N,inf);
-bool bellman(ll src)
+vector<bool>negCycle(N,false);
+ll n,m,q,s;
+void bellman(ll src)
 {
     dist[src]=0;
     for(ll i=1;i<=n;i++)
@@ -15,27 +17,55 @@ bool bellman(ll src)
         {
             ll u=child[0];
             ll v=child[1];
-            ll w=child[2];
+            ll w = child[2];
             if(dist[u]+w<dist[v]&&dist[u]!=inf)
             {
                 dist[v]=dist[u]+w;
-                if(i==n)return true;
+              
             }
         }
-    } return false;
+    }
+    for(ll i=1;i<=n;i++)
+    {
+        for(vector<ll>child : g)
+        {
+            ll u=child[0];
+            ll v=child[1];
+            ll w = child[2];
+            if(dist[u]+w<dist[v]&&dist[u]<inf)
+            {
+               negCycle[v]=true;
+            }
+             if (negCycle[u])
+                negCycle[v] = true;
+        }
+    }
+    
 }
 int main()
 {
-    cin>>n>>m;
+    bool input = true;
+    while(input)
+    {
+        cin>>n>>m>>q>>s;
+        if((n+m+q+s)==0 && s==0){input = false;return 0;}
+        g.clear();
+        fill(dist.begin(), dist.end(), inf);
+        fill(negCycle.begin(), negCycle.end(), false);
     for(ll i=0;i<m;i++)
     {
         ll u,v,w;
         cin>>u>>v>>w;
-       g.push_back({u,v,w});
+        g.push_back({u,v,w});
     }
-   
-    if(!bellman(1))
-    for(ll i=1;i<=n;i++)cout<<dist[i]<<" ";
-    else cout<<-1;
-    
+    bellman(s);
+    for(ll i=0;i<q;i++)
+    {
+        ll d;cin>>d;
+        if(dist[d]==inf)cout<<"Impossible"<<endl;
+        else if(negCycle[d]==true)cout<<"-Infinity"<<endl;
+        else cout<<dist[d]<<endl;
+    }
+    cout<<endl;
+}
 }
