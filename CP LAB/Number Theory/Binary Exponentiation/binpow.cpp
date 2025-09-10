2fa34c8319ee6a5d7c46c522e0bb321de9bec73b
Omor Sultan <145095872+omorsultan@users.noreply.github.com>
2025-09-10 08:15:20 +0600

    add modulas

--- CP LAB/Number Theory/Binary Exponentiation/binpow.cpp
+++ CP LAB/Number Theory/Binary Exponentiation/binpow.cpp
@@ -1,11 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+long long m = 1e9 + 7; // modulus
 
 long long binpow(long long a, long long b) {
     long long result = 1;
     while (b > 0) {
-        if (b & 1) result *= a; // multiply when bit is 1
-        a *= a; // square a
+        if (b & 1) result = (result * a) % m; // multiply when bit is 1
+        a = (a * a) % m; // square a
         b >>= 1; // divide b by 2
     }
     return result;
